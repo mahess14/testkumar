f0107438858b6a90b1dd16be079675fee333c08c
mahess14 <gmksvec14@gmail.com>
2023-04-09 23:11:50 +0530

    committing test6

---
+++ Test6.java
@@ -0,0 +1,16 @@
+package kumar;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+		float e=10.5f;
+		
+		int z=x+y+a+b+c;
+		System.out.println(e);
+		System.out.println(z);
+
+	}
+
+}
+
+
