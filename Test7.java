5e5e19c3a35ce49a92a60a4aae30101c24640baa
mahess14 <gmksvec14@gmail.com>
2023-04-09 23:22:53 +0530

    committing test7

---
+++ Test7.java
@@ -0,0 +1,21 @@
+Java if Statement
+The Java if statement tests the condition. It executes the if block if condition is true.
+Syntax:
+if(condition){  
+//code to be executed  
+}  
+
+ 
+
+Java if-else Statement
+The Java if-else statement also tests the condition. It executes the if block if condition is true otherwise else block is executed.
+Syntax:
+if(condition){  
+//code if condition is true  
+}else{  
+//code if condition is false  
+}  
+ 
+
+
+
