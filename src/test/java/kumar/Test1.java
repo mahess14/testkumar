745c3399222e784ad437cca2fbefc96f1b5816a4
mahess14 <128316289+mahess14@users.noreply.github.com>
2023-04-09 21:21:46 +0530

    Update Test1.java
    modified test1

--- src/test/java/kumar/Test1.java
+++ src/test/java/kumar/Test1.java
@@ -3,8 +3,8 @@ package kumar;
 public class Test1 {
 
 	public static void main(String[] args) {
-		int x=100;
-		int y=200;
+		int x=100+100;
+		int y=200+200;
 		int z=x+y;
 		System.out.println(z);
 
