b91554435b1f9a8a24aa64bd8754e9b4c0ffbe45
mahess14 <gmksvec14@gmail.com>
2023-04-09 21:25:26 +0530

    commtting2

---
+++ src/test/java/kumar/Test2.java
@@ -0,0 +1,15 @@
+package kumar;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int x=100;
+		int y=200;
+		int a=300;
+		int b=400;
+		int z=x+y;
+		System.out.println(z);
+
+	}
+
+}
