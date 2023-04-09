17d296e0231105601b74fb7cef7c8f63f826262c
mahess14 <gmksvec14@gmail.com>
2023-04-09 23:35:34 +0530

    committing test8

---
+++ Test8.java
@@ -0,0 +1,18 @@
+package kumar;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		float e=10.5f;
+		float e1=10.5f;
+		float e2=10.5f;
+		
+		System.out.println(e);
+		System.out.println(e1);
+		System.out.println(e2);
+	
+
+	}
+
+}
+
